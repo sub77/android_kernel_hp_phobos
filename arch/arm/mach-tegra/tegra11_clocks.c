117647ed8a12dcb802b9928ab46b8cd1ffa55c6f
Quaesar <ratikrk@gmail.com>
2016-02-20 14:26:02 -0700

    phobos: Overclock to 2.55GHz

--- arch/arm/mach-tegra/tegra11_clocks.c
+++ arch/arm/mach-tegra/tegra11_clocks.c
@@ -6045,7 +6045,7 @@ static struct clk tegra_dfll_cpu = {
 	.flags     = DFLL,
 	.ops       = &tegra_dfll_ops,
 	.reg	   = 0x2f4,
-	.max_rate  = 2000000000,
+	.max_rate  = 3000000000U,
 };
 
 static struct clk tegra_pll_re_vco = {
@@ -6330,7 +6330,7 @@ static struct clk tegra_clk_cclk_g = {
 	.inputs	= mux_cclk_g,
 	.reg	= 0x368,
 	.ops	= &tegra_super_ops,
-	.max_rate = 2000000000,
+	.max_rate = 3000000000U,
 };
 
 static struct clk tegra_clk_cclk_lp = {
@@ -6355,7 +6355,7 @@ static struct clk tegra_clk_virtual_cpu_g = {
 	.name      = "cpu_g",
 	.parent    = &tegra_clk_cclk_g,
 	.ops       = &tegra_cpu_ops,
-	.max_rate  = 2000000000,
+	.max_rate  = 3000000000U,
 	.u.cpu = {
 		.main      = &tegra_pll_x,
 		.backup    = &tegra_pll_p_out4,
@@ -6386,7 +6386,7 @@ static struct clk tegra_clk_cpu_cmplx = {
 	.name      = "cpu",
 	.inputs    = mux_cpu_cmplx,
 	.ops       = &tegra_cpu_cmplx_ops,
-	.max_rate  = 2000000000,
+	.max_rate  = 3000000000U,
 };
 
 static struct clk tegra_clk_cop = {
